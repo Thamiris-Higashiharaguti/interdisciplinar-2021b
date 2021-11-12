0341f5e7c1c9a1b953c04f99f566adc5a9dbab09
Matheus Amaro Santos <matheusamaros@hotmail.com>
2021-11-12 17:59:26 -0300

    Login funcionando

--- tiro-guerra/Repositories/Atirador/AtiradorRepository.cs
+++ tiro-guerra/Repositories/Atirador/AtiradorRepository.cs
@@ -177,7 +177,58 @@ namespace TiroGuerra.Repositories
             finally {
                 Dispose();
             }
-        }       
+        } 
+
+        public Atirador Read(string CPF, string RA)
+        {
+            try 
+            {
+                Console.WriteLine(CPF);
+                Atirador atirador = new Atirador();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+
+                cmd.CommandText = "SELECT U.id, U.Nome, U.CPF, U.RG,A.Formacao, A.RA, A.Numero, P.Nome "+
+                "FROM TB_Atirador A INNER Join TB_Usuario  U ON A.Id_Usuario = U.Id Inner Join TB_Pelotao P "+
+                "ON A.Id_Pelotao = P.Id Where U.CPF = @CPF AND A.RA = @RA";
+
+                cmd.Parameters.AddWithValue("@CPF", CPF);
+                cmd.Parameters.AddWithValue("@RA", RA);
+                
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if(reader.Read()) 
+                {
+                    
+                    atirador.Id = reader.GetInt32(0);
+                    atirador.Nome = reader.GetString(1);
+                    atirador.CPF = reader.GetString(2);
+                    atirador.RG = reader.GetString(3);
+                    atirador.Formacao = reader.GetString(4);
+                    atirador.RA = reader.GetString(5);
+                    atirador.Numero = reader.GetString(6);
+                    
+
+                    atirador.Pelotao = new Pelotao {
+                        Nome = reader.GetString(7)
+                    };
+
+                    return atirador;
+                }
+
+                return null;
+                
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("Atirador não encontrada.");
+            }
+            finally {
+                Dispose();
+            }
+        }             
     }
 }
 
