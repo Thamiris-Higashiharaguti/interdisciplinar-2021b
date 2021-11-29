3a5f162701b5e4aee1a9d654f97cc3bc9cac4c0c
Matheus <74687220+MatheusAmaros@users.noreply.github.com>
2021-11-29 20:19:43 -0300

    Login e guarnição

---
+++ tiro-guerra/Models/AtiradorGuarda.cs
@@ -0,0 +1,13 @@
+using System;
+namespace TiroGuerra.Models
+{
+    //atirador
+    public class AtiradorGuarda
+    {
+        public int IdAtirador { get; set; }
+
+        public int IdGuarda { get; set; }
+
+        public string NomeAtirador { get; set; }
+    }
+}
--- tiro-guerra/Repositories/Guarda/IGuardaRepository.cs
+++ tiro-guerra/Repositories/Guarda/IGuardaRepository.cs
@@ -1,17 +1,26 @@
 using System;
 using System.Collections.Generic;
 using TiroGuerra.Models;
-
 namespace TiroGuerra.Repositories
 {
     public interface IGuardaRepository
     {
-        void Create(Guarda model);
+        void Create(List<Guarda> guardas, List<Guarnicao> guarnicoes);
 
-        Guarda Read(int id);
+        List<Guarda> Read(DateTime domingo, DateTime sabado);
 
-        void Update(int id, Guarda model);
+        void Update(List<Guarda> model);
 
         void Delete(int id);
     }
+
+    
+          
+            
+    
+
+          
+    
+    
+  
 }
